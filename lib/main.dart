3e71411110977ccf74d8320db14aca4d6599c15e
abir3262 <hasan15-5459@diu.edu.bd>
2025-02-25 11:31:05 +0600

    add drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,6 +12,22 @@ class LabClass05 extends StatelessWidget {
     return MaterialApp(
       title: 'Basic Flutter UI-02',
       home: Scaffold(
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              ListTile(
+                leading: Icon(Icons.image),
+                title: Text("Image"),
+                onTap: (){},
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text("Settings"),
+                onTap: (){},
+              ),
+            ],
+          ),
+        ),
         appBar: AppBar(
           title: Text("Basic Flutter UI-02"),
           backgroundColor: const Color.fromARGB(255, 21, 59, 224),
@@ -20,10 +35,9 @@ class LabClass05 extends StatelessWidget {
         ),
         body: Row(
           children: [
-            CachedNetworkImage(
-              imageUrl: "https://images.pexels.com/photos/1229042/pexels-photo-1229042.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
-            
-            
+            Image.network(
+              "https://images.pexels.com/photos/1229042/pexels-photo-1229042.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            ), 
             Image.network(
               "https://images.pexels.com/photos/1496373/pexels-photo-1496373.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             ),
