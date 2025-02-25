aa0cd19b951300a3d85b95ae0f4bc4721420bcb1
EiaminHassan <shanto15-5251@diu.edu.bd>
2025-02-25 09:57:31 +0600

    previous code

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,80 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const SimpleNikeApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class SimpleNikeApp extends StatelessWidget {
+  const SimpleNikeApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home:Scaffold(
-        appBar: AppBar(
-          title: Text("Basic Ui"),
-          centerTitle: true,
-          elevation: 10.5,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color.fromARGB(236, 7, 69, 240), Color.fromARGB(255, 141, 168, 111)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  '50% OFF',
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  height: 200,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    image: const DecorationImage(
+                      image: NetworkImage('https://i.postimg.cc/05mb5106/1217812-800-800.webp'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'Nike Air',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  '\$15,000',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.yellowAccent,
+                    decoration: TextDecoration.lineThrough,
+                  ),
+                ),
+                const Text(
+                  '\$7,500 Only!',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
-        body: Text("Hello"),
       ),
     );
   }
 }
-
